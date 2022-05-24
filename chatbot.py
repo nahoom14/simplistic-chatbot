051ce780636de7beac3b4d0ea166dc86f56b33db
Nahoom Belete <nahoombelete22@gmail.com>
2022-05-24 13:35:56 +0200

    Add delay for output

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,29 @@
-import re
+import re, time
 
 bot_message = "Hello, my name is Diana. What is your name?"
 patient_message = "My name is John."
 
 print("Diana: " + bot_message )
+time.sleep(2)
 print("John: " + patient_message)
+time.sleep(2)
 
 print("Diana: " + re.sub("My name is", "Hi", patient_message))
+time.sleep(2)
 
 
 bot_message = "How are you feeling today?"
 patient_message = "I am feeling sad."
 
 print("Diana: " + bot_message)
+time.sleep(2)
 print("John: " + patient_message)
+time.sleep(2)
 
 print("Diana: " + re.sub("I am", "I am sorry you are", patient_message))
+time.sleep(2)
 print("Diana: " + re.sub(".* (depressed|sad)", "Why are you feeling \g<1>?", patient_message))
+time.sleep(2)
 
 patient_message = "I don't know what my purpose is in life."
 bot_message = re.sub("I", "You", patient_message)
@@ -26,4 +33,5 @@ bot_message += "?"
 
 
 print("John: " + patient_message)
+time.sleep(2)
 print("Diana: " + bot_message)
